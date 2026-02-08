c39e16dd9b42f919a294fc0af1a9b7dfe2480fa2
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-02-09 00:27:08 +0530

    js

--- FocusOnToday/script.js
+++ FocusOnToday/script.js
@@ -1,7 +1,16 @@
-const checkBoxList = document.querySelectorAll(".custom-checkbox")
- 
-checkBoxList.forEach((checkbox)=>{
-   checkbox.addEventListener("click",(e)=>{
-    console.log("clicked")
-   })
-})
+const checkBoxList = document.querySelectorAll(".custom-checkbox");
+const errorLabel = document.querySelector(".erroe-lebel");
+
+checkBoxList.forEach((checkbox) => {
+  checkbox.addEventListener("click", () => {
+    const goalContainer = checkbox.parentElement;
+    const input = goalContainer.querySelector(".goal-input");
+
+    if (input.value !== "") {
+      goalContainer.classList.toggle("completed");
+      errorLabel.style.display = "none";
+    } else {
+      errorLabel.style.display = "block";
+    }
+  });
+});
