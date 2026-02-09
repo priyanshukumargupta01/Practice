0d23a0bc09b3e106d55ee2bd256d6f117b6f5d6f
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-02-09 19:21:46 +0530

    completed mini project

--- FocusOnToday/script.js
+++ FocusOnToday/script.js
@@ -1,16 +1,31 @@
-const checkBoxList = document.querySelectorAll(".custom-checkbox");
-const errorLabel = document.querySelector(".erroe-lebel");
+// const checkBoxList = document.querySelectorAll(".custom-checkbox");
+// const errorLabel = document.querySelector(".erroe-lebel");
 
-checkBoxList.forEach((checkbox) => {
-  checkbox.addEventListener("click", () => {
-    const goalContainer = checkbox.parentElement;
-    const input = goalContainer.querySelector(".goal-input");
+// checkBoxList.forEach((checkbox) => {
+//   checkbox.addEventListener("click", () => {
+//     const goalContainer = checkbox.parentElement;
+//     const input = goalContainer.querySelector(".goal-input");
 
-    if (input.value !== "") {
-      goalContainer.classList.toggle("completed");
-      errorLabel.style.display = "none";
-    } else {
-      errorLabel.style.display = "block";
-    }
-  });
-});
+//     if (input.value !== "") {
+//       goalContainer.classList.toggle("completed");
+//       errorLabel.style.display = "none";
+//     } else {
+//       errorLabel.style.display = "block";
+//     }
+//   });
+// });
+
+
+const checkBoxList = document.querySelectorAll(".custom-checkbox")
+const inputFields = document.querySelectorAll(".goal-input")
+
+checkBoxList.forEach((checkbox)=>{ 
+      checkbox.addEventListener("click",(e)=>{
+        const allFieldsFilled = {...inputFields}.every(function(input){
+          return input.value
+        })
+        if(allFieldsFilled){ 
+        checkbox.parentElement.classList.toggle("completed") 
+        }
+      })
+})
